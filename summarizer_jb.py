9f9b7f4355d6e9659c9eb8e3a4d7b229e8a22665
Alex <alex@alex.alex>
2025-05-19 00:23:26 +0400

    changed logging level

--- summarizer_jb.py
+++ summarizer_jb.py
@@ -10,7 +10,7 @@ from openai import OpenAI
 from telethon import TelegramClient
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
 
 load_dotenv()
 
