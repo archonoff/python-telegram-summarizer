2fc97913dfbdd60af5006a1b1ea0edb1c5743c7a
archonoff <archonoff@gmail.com>
2025-05-21 04:01:39 +0400

    warn logger level

--- historizer.py
+++ historizer.py
@@ -8,7 +8,7 @@ from openai import OpenAI
 from models import ChatHistory
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 load_dotenv()
 
